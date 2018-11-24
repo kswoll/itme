446335978b230c9a6ba73ad9fd6867f0f64fc57c
Kirk Woll <kirk@plangrid.com>
2018-11-24 14:17:40 -0800

    fun

---
+++ ItMe/Pages/Logout.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using ItMe.Database;
+using ItMe.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ItMe.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly TokenManager tokenManager;
+
+        public LogoutModel(TokenManager tokenManager)
+        {
+            this.tokenManager = tokenManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            tokenManager.ProcessLogout();
+            return RedirectToPage("/Index");
+        }
+    }
+}
