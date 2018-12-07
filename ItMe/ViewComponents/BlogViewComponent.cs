74b007e408a64004ca67bc24ea874df50699261d
Kirk Woll <kirk@plangrid.com>
2018-12-06 19:21:56 -0800

    fix blog order

--- ItMe/ViewComponents/BlogViewComponent.cs
+++ ItMe/ViewComponents/BlogViewComponent.cs
@@ -19,7 +19,7 @@ namespace ItMe.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var host = HttpContext.Request.Host.Host;
-            var posts = await db.BlogPosts.Where(x => x.Person.Host == host).Select(Mappers.MapBlogPost).ToArrayAsync();
+            var posts = await db.BlogPosts.Where(x => x.Person.Host == host).OrderByDescending(x => x.Created).Select(Mappers.MapBlogPost).ToArrayAsync();
             var person = await db.Persons.Where(x => x.Host == host).Select(Mappers.MapPerson).SingleAsync();
 
             return View(new BlogViewComponentModel
