7bc9721ee7d7faa29684387e1007fc821fb1c3ed
Kirk Woll <kirk@plangrid.com>
2018-12-08 12:11:39 -0800

    fix xml namespace issue

--- ItMe/Pages/Sitemap.cshtml.cs
+++ ItMe/Pages/Sitemap.cshtml.cs
@@ -14,6 +14,8 @@ namespace ItMe.Pages
     {
         private readonly ItMeDb db;
 
+        private const string ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+
         public SitemapModel(ItMeDb db)
         {
             this.db = db;
@@ -21,7 +23,7 @@ namespace ItMe.Pages
 
         public async Task<IActionResult> OnGet()
         {
-            var urlset = new XElement("urlset");
+            var urlset = new XElement(XName.Get("urlset", ns));
             var document = new XDocument(urlset);
 
             var host = HttpContext.Request.Host.Host;
@@ -39,9 +41,9 @@ namespace ItMe.Pages
 
         private void AddUrl(XElement urlset, string url, DateTime lastModified)
         {
-            var urlElement = new XElement("url");
-            var locElement = new XElement("loc") { Value = $"https://kirkwoll.com/{url}" };
-            var lastmodElement = new XElement("lastmod") { Value = lastModified.ToString("yyyy-MM-dd") };
+            var urlElement = new XElement(XName.Get("url", ns));
+            var locElement = new XElement(XName.Get("loc", ns)) { Value = $"https://kirkwoll.com/{url}" };
+            var lastmodElement = new XElement(XName.Get("lastmod", ns)) { Value = lastModified.ToString("yyyy-MM-dd") };
             urlElement.Add(locElement);
             urlElement.Add(lastmodElement);
             urlset.Add(urlElement);
